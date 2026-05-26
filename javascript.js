49786657f8c1bec1eecde762641e359489e03541
ShreejeshG12 <129910787+ShreejeshG12@users.noreply.github.com>
2026-05-25 22:31:03 -0400

    Functional programming. Used 100vh for screensize on body.

--- javascript.js
+++ javascript.js
@@ -1,6 +1,19 @@
 // Add DOM elements inside a div container to create a square of as many boxes as the user inputs using prompt
 const container = document.querySelector(".container");
 
+function getGridSize() {
+    let gridSize;
+    while (true) {
+        gridSize = Number(prompt("What size board do you want (1-100)"));
+
+        if (gridSize > 0 && gridSize <= 100) {
+            return gridSize;
+        } 
+        
+    }
+    
+}
+
 function createAGrid(N) {
     container.innerHTML = "";
     for (let i = 0; i < N; i++) {
@@ -16,12 +29,15 @@ function createAGrid(N) {
     }
 };
 
-createAGrid(16)
+createAGrid(45)
 
 const cells = document.querySelectorAll(".column");
 
 cells.forEach(cell => {
     cell.addEventListener("mouseover", () => {
-        cell.style.backgroundColor = "navy";
+        const r = Math.floor(Math.random() * 256);
+        const g = Math.floor(Math.random() * 256);
+        const b = Math.floor(Math.random() * 256);
+        cell.style.backgroundColor = `rgb(${r},${g},${b})`;
     })
-})
+});
